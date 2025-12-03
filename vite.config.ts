430d50c26f3e72851c7a878b5f84a7ca5aee9b66
Will Bowman <gcphost@gmail.com>
2025-12-02 18:24:38 -0800

    Update vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/cat-loader/" : "/",
+  base: "/cat-loader/",
   build: {
     outDir: "dist",
   },
